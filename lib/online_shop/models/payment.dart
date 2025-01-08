b6ead0da9c3bb0e882d263852788bcf5331f16da
DanielNiko30 <danielniko300703@gmail.com>
2025-01-09 00:04:25 +0700

    ..

--- lib/online_shop/models/payment.dart
+++ lib/online_shop/models/payment.dart
@@ -3,15 +3,13 @@ import 'dart:convert';
 import 'customer_detail.dart';
 
 class Payment {
-  final String orderId;
-  final double totalAmount;
+  final String token;
   final String paymentUrl;
   final String cartId; // Menambahkan cartId
   final CustomerDetails customerDetails; // Menambahkan customerDetails
 
   Payment({
-    required this.orderId,
-    required this.totalAmount,
+    required this.token,
     required this.paymentUrl,
     required this.cartId,
     required this.customerDetails,
@@ -20,19 +18,17 @@ class Payment {
   // Factory method untuk membuat objek Payment dari JSON map
   factory Payment.fromJson(Map<String, dynamic> json) {
     return Payment(
-      orderId: json['order_id'] ?? '',
-      totalAmount: json['total_amount']?.toDouble() ?? 0.0,
-      paymentUrl: json['payment_url'] ?? '',
-      cartId: json['cart_id'] ?? '', // Menambahkan pengambilan cartId
-      customerDetails: CustomerDetails.fromJson(json['customerDetails'] ?? {}), // Menambahkan pengambilan customerDetails
+      token: json['token'] ?? '',
+      paymentUrl: json['redirect_url'] ?? '',
+      cartId: json['cart_id'] ?? '',
+      customerDetails: CustomerDetails.fromJson(json['customerDetails'] ?? {}),
     );
   }
 
   // Method untuk mengonversi objek Payment ke JSON map
   Map<String, dynamic> toJson() {
     return {
-      'order_id': orderId,
-      'total_amount': totalAmount,
+      'token': token,
       'payment_url': paymentUrl,
       'cart_id': cartId, // Menambahkan cartId ke dalam JSON
       'customerDetails': customerDetails.toJson(), // Menambahkan customerDetails ke dalam JSON
